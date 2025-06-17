078e3d99a13535441f8f11be6587f9644c40a8c7
Samuel Pena <smlpnsz.pena@gmail.com>
2025-06-17 11:57:22 -0300

    Configurações para deploy no GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
 import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
+import ProductListingPage from './pages/ProductListingPage';
+import ProductViewPage from './pages/ProductViewPage';
 import CategoriesPage from './pages/CategoriesPage';
 import OrdersPage from './pages/OrdersPage';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/orders" element={<OrdersPage />} />
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductListingPage />} />
+        <Route path="/product/:id" element={<ProductViewPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
         </Routes>
-      </Layout>
-    </Router>
+    </Layout>
   );
 }
 
